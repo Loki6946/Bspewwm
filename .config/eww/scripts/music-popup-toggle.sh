0fd115b995f0d42c3e82c556112b30c24746858c
loki6946 <aldianderson6@gmail.com>
2026-04-30 17:29:23 +0700

    add music player popup

---
+++ .config/eww/scripts/music-popup-toggle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+state=$(eww get music-popup-open)
+
+open_music() {
+  if ! eww active-windows | grep -q "music-popup"; then
+    eww open music-popup
+  fi
+  eww update music-popup-open=true
+}
+
+close_music() {
+  eww close music-popup
+  eww update music-popup-open=false
+}
+
+case $1 in
+  close)
+    close_music
+    exit 0;;
+esac
+
+case $state in
+  true)
+    close_music;;
+  false)
+    open_music;;
+esac
