b02d54e07ecdb5777f577126d1f33aa0bfa44669
loki6946 <aldianderson6@gmail.com>
2026-05-16 08:23:03 +0700

    clean things

--- .config/eww/scripts/screenshot-cycle-delay.sh
+++ .config/eww/scripts/screenshot-cycle-delay.sh
@@ -2,9 +2,8 @@
 current=$(eww get screenshot-delay)
 
 case $current in
-  0) eww update screenshot-delay=1 ;;
   1) eww update screenshot-delay=3 ;;
   3) eww update screenshot-delay=5 ;;
-  5) eww update screenshot-delay=0 ;;
-  *) eww update screenshot-delay=0 ;;
+  5) eww update screenshot-delay=1 ;;
+  *) eww update screenshot-delay=1 ;;
 esac
