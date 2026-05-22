f0abc7251ad1f7be87deab33ee79d76275aec320
loki6946 <aldianderson6@gmail.com>
2026-05-22 13:35:39 +0700

    make it more seamless

--- .config/eww/scripts/theme-switch.sh
+++ .config/eww/scripts/theme-switch.sh
@@ -2,18 +2,25 @@
 
 theme=$1
 COLOR_FILE="$HOME/.config/eww/_color.scss"
+get_color="$HOME/.config/bspwm/bspwm.d/get-color.sh"
 
 # swap active theme
 sed -i "s|@import \"themes/.*\"|@import \"themes/${theme}\"|" "$COLOR_FILE"
 
 # wait before continue
-sleep 1.5
+sleep 1
 
 # update eww active theme
 eww update active-theme="$theme"
 
-# Resync sysinfo popup
+# resync sysinfo popup variable
 $HOME/.config/eww/scripts/sysinfo-popup-toggle.sh
 
+# apply colors to xcorners and bspwm
+killall -9 xcorners
+xcorners -y 30 -r 16 -c $($get_color background)FF -1
+bspc config normal_border_color \#$($get_color on-surface-dim)
+bspc config focused_border_color \#$($get_color accent)
+
 # reload bspwm — handles border colors and xcorners automatically
-bspc wm -r
+# bspc wm -r
