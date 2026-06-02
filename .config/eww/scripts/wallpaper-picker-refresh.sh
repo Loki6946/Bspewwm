ec3dfd0add608fe89a98538279aaf99ef0b2c2a4
loki6946 <aldianderson6@gmail.com>
2026-06-02 10:37:47 +0700

    add refresh state

--- .config/eww/scripts/wallpaper-picker-refresh.sh
+++ .config/eww/scripts/wallpaper-picker-refresh.sh
@@ -1,11 +1,13 @@
+eww update wallpaper-refreshing=true
 eww poll wallpaper-list
 bash ~/.config/eww/scripts/wallpaper-thumbs-generate.sh
 
-sleep 2
+sleep 3
 
 # close and reopen picker to refresh
 if eww active-windows | grep -q "wallpaper-picker"; then
   eww close wallpaper-picker
   sleep 0.3
+  eww update wallpaper-refreshing=false
   eww open wallpaper-picker
 fi
